225a01014d8be862657adee9c9e69d88524501d2
Hao Yeh <88352138+jackromeo0891111@users.noreply.github.com>
2023-01-26 13:23:11 -0800

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -6,10 +6,11 @@ var hasMoney: Int = 550
 var hasCups: Int = 9
 
 fun buy() {
-    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
     when (readln()) {
         "1" -> {
             if (hasWater > 250 && hasBeans > 16 && hasCups > 0) {
+                println("I have enough resources, making you a coffee!")
                 hasWater -= 250
                 hasBeans -= 16
                 hasCups -= 1
@@ -19,6 +20,7 @@ fun buy() {
 
         "2" -> {
             if (hasWater > 350 && hasMilk > 75 && hasBeans > 20 && hasCups > 0) {
+                println("I have enough resources, making you a coffee!")
                 hasWater -= 350
                 hasMilk -= 75
                 hasBeans -= 20
@@ -28,6 +30,7 @@ fun buy() {
         }
         "3" -> {
             if (hasWater > 200 && hasMilk > 100 && hasBeans > 12 && hasCups > 0) {
+                println("I have enough resources, making you a coffee!")
                 hasWater -= 200
                 hasMilk -= 100
                 hasBeans -= 12
@@ -35,7 +38,8 @@ fun buy() {
                 hasMoney += 6
             } else println("Insufficient resource")
         }
-        else -> println("Invalid input")
+        "back" -> main()
+        else -> main()
     }
 }
 
@@ -59,22 +63,25 @@ fun take(){
     hasMoney = 0
 }
 
+fun remaining(){
+    println("The coffee machine has:")
+    println("$hasWater ml of water")
+    println("$hasMilk ml of milk")
+    println("$hasBeans g of coffee beans")
+    println("$hasCups disposable cups")
+    println("\$$hasMoney of money")
+}
+
 fun main() {
     var isOn = true
     while(isOn){
-        println("The coffee machine has:")
-        println("$hasWater ml of water")
-        println("$hasMilk ml of milk")
-        println("$hasBeans g of coffee beans")
-        println("$hasCups disposable cups")
-        println("\$$hasMoney of money")
-        println("Write action (buy, fill, take): ")
+        println("Write action (buy, fill, take, remaining, exit): ")
         when(readln()){
             "buy" -> buy()
             "fill" -> fill()
             "take" -> take()
+            "remaining" -> remaining()
             "exit" -> isOn = false
-            else -> println("invalid input")
         }
     }
 }
